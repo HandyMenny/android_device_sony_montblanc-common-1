3ff2cc3b322e8c2910df01ff8cc0ffd0e6cf8432
Agontuk <rifat662@gmail.com>
2015-02-19 13:28:12 +0600

    Enable TARGET_NEEDS_BIONIC_MD5 flag
    Needed for suntrold binary

--- BoardConfigCommon.mk
+++ BoardConfigCommon.mk
@@ -18,8 +18,9 @@ TARGET_GLOBAL_CPPFLAGS += -mfpu=neon -mfloat-abi=softfp
 TARGET_ARCH_VARIANT := armv7-a-neon
 ARCH_ARM_HAVE_TLS_REGISTER := true
 
-# Restore pre-lollipop mmap behavior
+# Bionic flags
 BOARD_USES_LEGACY_MMAP := true
+TARGET_NEEDS_BIONIC_MD5 := true
 
 # RIL implementation
 BOARD_RIL_CLASS := ../../../device/sony/montblanc-common/telephony-common/
